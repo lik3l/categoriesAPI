60e443cbaedaebacff32dee3bb78d2be4055ee41
Ihor Magur <ihor.magur@devabit.com>
2020-01-21 08:07:31 +0200

    Added create method and view

--- categories/serializers.py
+++ categories/serializers.py
@@ -3,7 +3,36 @@ from rest_framework import serializers
 from categories.models import Category
 
 
+def save_categories(categories, initial_parent=None):
+    instances = list()
+    for category in categories:
+        children = category.pop('children', None)
+        parent = Category.objects.create(**category, parent=initial_parent)
+        serialized = CategorySerializer(parent).data
+        if children:
+            serialized['children'] = save_categories(children, parent)
+        instances.append(serialized)
+
+    return instances
+
+
 class CategorySerializer(serializers.ModelSerializer):
+    children = serializers.JSONField(required=False, write_only=True)
+
     class Meta:
         model = Category
-        fields = ('pk', 'name')
+        fields = ('id', 'name', 'children')
+
+    def validate_children(self, children):
+        serializer = CategorySerializer(data=children, many=True, context=self.context)
+        serializer.is_valid(raise_exception=True)
+        return serializer.validated_data
+
+    def create(self, validated_data):
+        children = validated_data.pop('children')
+        instance = Category.objects.create(**validated_data)
+        resp = CategorySerializer(instance).data
+        if children:
+            resp['children'] = save_categories(children, instance)
+
+        return resp
--- categories/views.py
+++ categories/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from categories.models import Category
 from categories.serializers import CategorySerializer
@@ -7,3 +8,10 @@ from categories.serializers import CategorySerializer
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.save()
+        headers = self.get_success_headers(serializer.data)
+        return Response(data, status=status.HTTP_201_CREATED, headers=headers)
