455199510c29f4baffbe23b1f4bec6917dfbf771
Ihor Magur <ihor.magur@devabit.com>
2020-01-21 11:14:06 +0200

    Added home redirects

--- categoriesAPI/urls.py
+++ categoriesAPI/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.shortcuts import redirect
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from categories.views import CategoryViewSet
@@ -22,6 +23,8 @@ router = DefaultRouter(trailing_slash=False)
 router.register('categories', CategoryViewSet)
 
 urlpatterns = [
+    path('', lambda r: redirect('/api/v1/categories')),
+    path('api/', lambda r: redirect('/api/v1/')),
     path('api/v1/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls'))
 ]
